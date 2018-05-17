6be008c34638a104ba4ab16c8c4436fa8f49f995
zouming.zm <zouming.zm@alibaba-inc.com>
2018-05-17 19:42:59 +0800

    init bridge_impl_iOS files

--- weex_core/Source/iOS/bridge/bridge_impl_ios.cpp
+++ weex_core/Source/iOS/bridge/bridge_impl_ios.cpp
@@ -18,3 +18,117 @@
  */
 
 #include "bridge_impl_ios.h"
+
+namespace WeexCore {
+    
+    Bridge_Impl_iOS *Bridge_Impl_iOS::m_instance = nullptr;
+    
+    Bridge_Impl_iOS::Bridge_Impl_iOS() {}
+    
+    Bridge_Impl_iOS::~Bridge_Impl_iOS() {}
+    
+    void Bridge_Impl_iOS::setJSVersion(const char* version) {
+        
+    };
+    
+    void Bridge_Impl_iOS::reportException(const char* pageId, const char *func, const char *exception_string) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callNative(const char* pageId, const char *task, const char *callback) {
+        
+    }
+    
+    void* Bridge_Impl_iOS::callNativeModule(const char* pageId, const char *module, const char *method,
+                                            const char *argString, const char *optString) {
+        
+    }
+    
+    void Bridge_Impl_iOS::callNativeComponent(const char* pageId, const char* ref, const char *method,
+                             const char *argString, const char *optString) {
+        
+    }
+    
+    void Bridge_Impl_iOS::setTimeout(const char* callbackID, const char* time) {
+        
+    }
+    
+    void Bridge_Impl_iOS::callNativeLog(const char* str_array) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callUpdateFinish(const char* pageId, const char *task, const char *callback) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callRefreshFinish(const char* pageId, const char *task, const char *callback) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callAddEvent(const char* pageId, const char* ref, const char *event) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callRemoveEvent(const char* pageId, const char* ref, const char *event) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callCreateBody(const char* pageId, const char *componentType, const char* ref,
+                       std::map<std::string, std::string> *styles,
+                       std::map<std::string, std::string> *attributes,
+                       std::set<std::string> *events,
+                       const WXCoreMargin &margins,
+                       const WXCorePadding &paddings,
+                       const WXCoreBorderWidth &borders) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callAddElement(const char* pageId, const char *componentType, const char* ref,
+                       int &index, const char* parentRef,
+                       std::map<std::string, std::string> *styles,
+                       std::map<std::string, std::string> *attributes,
+                       std::set<std::string> *events,
+                       const WXCoreMargin &margins,
+                       const WXCorePadding &paddings,
+                       const WXCoreBorderWidth &borders,
+                       bool willLayout) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callLayout(const char* pageId, const char* ref,
+                   int top, int bottom, int left, int right,
+                   int height, int width, int index) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callUpdateStyle(const char* pageId, const char* ref,
+                        std::vector<std::pair<std::string, std::string>> *style,
+                        std::vector<std::pair<std::string, std::string>> *margin,
+                        std::vector<std::pair<std::string, std::string>> *padding,
+                        std::vector<std::pair<std::string, std::string>> *border) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callUpdateAttr(const char* pageId, const char* ref,
+                       std::vector<std::pair<std::string, std::string>> *attrs) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callCreateFinish(const char* pageId) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callRemoveElement(const char* pageId, const char* ref) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callMoveElement(const char* pageId, const char* ref, const char* parentRef, int index) {
+        
+    }
+    
+    int Bridge_Impl_iOS::callAppendTreeCreateFinish(const char* pageId, const char* ref) {
+        
+    }
+    
+}
+
--- weex_core/Source/iOS/bridge/bridge_impl_ios.h
+++ weex_core/Source/iOS/bridge/bridge_impl_ios.h
@@ -20,6 +20,98 @@
 #ifndef bridge_impl_ios_h
 #define bridge_impl_ios_h
 
+#include <core/bridge/bridge.h>
 #include <stdio.h>
+
+namespace WeexCore {
+    class Bridge_Impl_iOS : public Bridge {
+    private:
+    public:
+        static Bridge_Impl_iOS *m_instance;
+        
+        //just to release singleton object
+        class Garbo {
+        public:
+            ~Garbo() {
+                if (Bridge_Impl_iOS::m_instance) {
+                    delete Bridge_Impl_iOS::m_instance;
+                }
+            }
+        };
+        
+        Bridge_Impl_iOS();
+        ~Bridge_Impl_iOS();
+        
+        static Bridge_Impl_iOS *getInstance() {
+            if (m_instance == nullptr) {
+                m_instance = new Bridge_Impl_iOS();
+            }
+            return m_instance;
+        }
+        
+        void setJSVersion(const char* version) = 0;
+        
+        void reportException(const char* pageId, const char *func, const char *exception_string) = 0;
+        
+        int callNative(const char* pageId, const char *task, const char *callback) = 0;
+        
+        void* callNativeModule(const char* pageId, const char *module, const char *method,
+                                       const char *argString, const char *optString) = 0;
+        
+        void callNativeComponent(const char* pageId, const char* ref, const char *method,
+                                         const char *argString, const char *optString) = 0;
+        
+        void setTimeout(const char* callbackID, const char* time) = 0;
+        
+        void callNativeLog(const char* str_array) = 0;
+        
+        int callUpdateFinish(const char* pageId, const char *task, const char *callback) = 0;
+        
+        int callRefreshFinish(const char* pageId, const char *task, const char *callback) = 0;
+        
+        int callAddEvent(const char* pageId, const char* ref, const char *event) = 0;
+        
+        int callRemoveEvent(const char* pageId, const char* ref, const char *event) = 0;
+        
+        int callCreateBody(const char* pageId, const char *componentType, const char* ref,
+                                   std::map<std::string, std::string> *styles,
+                                   std::map<std::string, std::string> *attributes,
+                                   std::set<std::string> *events,
+                                   const WXCoreMargin &margins,
+                                   const WXCorePadding &paddings,
+                                   const WXCoreBorderWidth &borders) = 0;
+        
+        int callAddElement(const char* pageId, const char *componentType, const char* ref,
+                                   int &index, const char* parentRef,
+                                   std::map<std::string, std::string> *styles,
+                                   std::map<std::string, std::string> *attributes,
+                                   std::set<std::string> *events,
+                                   const WXCoreMargin &margins,
+                                   const WXCorePadding &paddings,
+                                   const WXCoreBorderWidth &borders,
+                                   bool willLayout= true) = 0;
+        
+        int callLayout(const char* pageId, const char* ref,
+                               int top, int bottom, int left, int right,
+                               int height, int width, int index) = 0;
+        
+        int callUpdateStyle(const char* pageId, const char* ref,
+                                    std::vector<std::pair<std::string, std::string>> *style,
+                                    std::vector<std::pair<std::string, std::string>> *margin,
+                                    std::vector<std::pair<std::string, std::string>> *padding,
+                                    std::vector<std::pair<std::string, std::string>> *border) = 0;
+        
+        int callUpdateAttr(const char* pageId, const char* ref,
+                                   std::vector<std::pair<std::string, std::string>> *attrs) = 0;
+        
+        int callCreateFinish(const char* pageId) = 0;
+        
+        int callRemoveElement(const char* pageId, const char* ref) = 0;
+        
+        int callMoveElement(const char* pageId, const char* ref, const char* parentRef, int index) = 0;
+        
+        int callAppendTreeCreateFinish(const char* pageId, const char* ref) = 0;
+    };
+}
 
 #endif /* bridge_impl_ios_h */
